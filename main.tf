9e4a4e7e701a2b1d90400b4edd0932178b8d5246
nikhiljsk <nikhiljsk98@gmail.com>
2022-02-18 04:38:33 +0530

    Nikhi JSK

--- main.tf
+++ main.tf
@@ -2,6 +2,17 @@ provider "aws" {
   region = "us-east-1"
 }
 
+# Key Pair
+resource "tls_private_key" "keypair" {
+  algorithm = "RSA"
+  rsa_bits  = 4096
+}
+
+resource "aws_key_pair" "keypair" {
+  key_name   = "keypair"
+  public_key = tls_private_key.keypair.public_key_openssh
+}
+
 # VPC
 resource "aws_vpc" "main" {
   cidr_block = "10.0.0.0/16"
@@ -13,8 +24,8 @@ resource "aws_vpc" "main" {
 
 # Subnets - 6
 resource "aws_subnet" "public_a" {
-  vpc_id     = aws_vpc.main.id
-  cidr_block = "10.0.1.0/24"
+  vpc_id            = aws_vpc.main.id
+  cidr_block        = "10.0.1.0/24"
   availability_zone = "us-east-1a"
 
   tags = {
@@ -23,8 +34,8 @@ resource "aws_subnet" "public_a" {
 }
 
 resource "aws_subnet" "private_a" {
-  vpc_id     = aws_vpc.main.id
-  cidr_block = "10.0.15.0/24"
+  vpc_id            = aws_vpc.main.id
+  cidr_block        = "10.0.15.0/24"
   availability_zone = "us-east-1a"
 
   tags = {
@@ -33,8 +44,8 @@ resource "aws_subnet" "private_a" {
 }
 
 resource "aws_subnet" "public_b" {
-  vpc_id     = aws_vpc.main.id
-  cidr_block = "10.0.2.0/24"
+  vpc_id            = aws_vpc.main.id
+  cidr_block        = "10.0.2.0/24"
   availability_zone = "us-east-1b"
 
   tags = {
@@ -43,8 +54,8 @@ resource "aws_subnet" "public_b" {
 }
 
 resource "aws_subnet" "private_b" {
-  vpc_id     = aws_vpc.main.id
-  cidr_block = "10.0.16.0/24"
+  vpc_id            = aws_vpc.main.id
+  cidr_block        = "10.0.16.0/24"
   availability_zone = "us-east-1b"
 
   tags = {
@@ -53,24 +64,24 @@ resource "aws_subnet" "private_b" {
 }
 
 resource "aws_subnet" "public_c" {
-    vpc_id     = aws_vpc.main.id
-    cidr_block = "10.0.3.0/24"
-    availability_zone = "us-east-1c"
-  
-    tags = {
-      Name = "Public Subnet C"
-    }
+  vpc_id            = aws_vpc.main.id
+  cidr_block        = "10.0.3.0/24"
+  availability_zone = "us-east-1c"
+
+  tags = {
+    Name = "Public Subnet C"
   }
-  
-  resource "aws_subnet" "private_c" {
-    vpc_id     = aws_vpc.main.id
-    cidr_block = "10.0.17.0/24"
-    availability_zone = "us-east-1c"
-  
-    tags = {
-      Name = "Private Subnet C"
-    }
+}
+
+resource "aws_subnet" "private_c" {
+  vpc_id            = aws_vpc.main.id
+  cidr_block        = "10.0.17.0/24"
+  availability_zone = "us-east-1c"
+
+  tags = {
+    Name = "Private Subnet C"
   }
+}
 
 # Internet Gateway for VPC
 resource "aws_internet_gateway" "igw" {
@@ -218,4 +229,211 @@ resource "aws_route_table_association" "private_b" {
 resource "aws_route_table_association" "private_c" {
   subnet_id      = aws_subnet.private_c.id
   route_table_id = aws_route_table.private_c.id
+}
+
+
+# Target Group
+resource "aws_lb_target_group" "target_s1" {
+  name     = "target-s1"
+  port     = 80
+  protocol = "HTTP"
+  vpc_id   = aws_vpc.main.id
+
+  tags = {
+    Name = "Target Group S1"
+  }
+}
+
+# Security Group for ALB
+resource "aws_security_group" "allow_http" {
+  name        = "allow_http"
+  description = "allow_http"
+  vpc_id      = aws_vpc.main.id
+
+  ingress {
+    description      = "HTTP"
+    from_port        = 80
+    to_port          = 80
+    protocol         = "HTTP"
+    cidr_blocks      = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "allow_http"
+  }
+}
+
+# Application Load Balancer
+resource "aws_lb" "alb_s1" {
+  name               = "ALB S1"
+  internal           = false
+  load_balancer_type = "application"
+  security_groups    = [aws_security_group.allow_http.id]
+  subnets            = ["us-east-1a", "us-east-1b", "us-east-1c"]
+
+  tags = {
+    "Name" = "ALB for S1"
+  }
+}
+
+# Attach ALB to Target Group
+resource "aws_alb_listener" "alb_listen" {
+  load_balancer_arn = "${aws_alb.alb_s1.arn}"
+  port              = "80"
+  protocol          = "HTTP"
+
+  default_action {
+    target_group_arn = "${aws_lb_target_group.target_s1.arn}"
+    type             = "forward"
+  }
+}
+
+# Auto Scaling Groups
+resource "aws_launch_template" "Template Services" {
+  name_prefix   = "template"
+  image_id      = "ami-033b95fb8079dc481"
+  instance_type = "t2.micro"
+  key_name      = aws_key_pair.keypair.keypair
+
+  tags = {
+    Name = "Template"
+  }
+}
+
+resource "aws_autoscaling_group" "ASG S1" {
+  vpc_zone_identifier = [aws_subnet.private_a.id, aws_subnet.private_b.id, aws_subnet.private_c.id]
+  desired_capacity    = 3
+  max_size            = 6
+  min_size            = 3
+  target_group_arns = ["${aws_lb_target_group.target_s1.arn}"]
+
+  launch_template {
+    id      = aws_launch_template.template.id
+    version = "$Latest"
+  }
+  
+  tags = {
+    Name = "ASG S1"
+  }
+}
+
+resource "aws_autoscaling_group" "ASG S2" {
+  vpc_zone_identifier = [aws_subnet.private_a.id, aws_subnet.private_b.id, aws_subnet.private_c.id]
+  desired_capacity    = 3
+  max_size            = 6
+  min_size            = 3
+
+  launch_template {
+    id      = aws_launch_template.template.id
+    version = "$Latest"
+  }
+  
+  tags = {
+    Name = "ASG S2"
+  }
+}
+
+resource "aws_autoscaling_group" "ASG S3" {
+  vpc_zone_identifier = [aws_subnet.private_a.id, aws_subnet.private_b.id, aws_subnet.private_c.id]
+  desired_capacity    = 3
+  max_size            = 6
+  min_size            = 3
+
+  launch_template {
+    id      = aws_launch_template.template.id
+    version = "$Latest"
+  }
+  
+  tags = {
+    Name = "ASG S3"
+  }
+}
+
+resource "aws_autoscaling_group" "ASG S4" {
+  vpc_zone_identifier = [aws_subnet.private_a.id, aws_subnet.private_b.id, aws_subnet.private_c.id]
+  desired_capacity    = 3
+  max_size            = 6
+  min_size            = 3
+
+  launch_template {
+    id      = aws_launch_template.template.id
+    version = "$Latest"
+  }
+  
+  tags = {
+    Name = "ASG S4"
+  }
+}
+
+# SG to allow SSH to Bastion Hosts
+resource "aws_security_group" "allow_ssh_pub" {
+  name        = "allow_ssh"
+  description = "Allow SSH"
+  vpc_id      = aws_vpc.main.id
+
+  ingress {
+    description = "SSH from the internet"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "allow_ssh_pub"
+  }
+}
+
+# Bastion Hosts
+resource "aws_instance" "bastion_a" {
+  ami           = "ami-033b95fb8079dc481"
+  associate_public_ip_address = true
+  instance_type = "t2.micro"
+  subnet_id                   = aws_subnet.public_a.id
+  vpc_security_group_ids      = [aws_security_group.allow_ssh_pub.id]
+  key_name      = aws_key_pair.keypair.keypair
+
+  tags = {
+    Name = "Bastion Host A"
+  }
+}
+
+resource "aws_instance" "bastion_b" {
+  ami           = "ami-033b95fb8079dc481"
+  associate_public_ip_address = true
+  instance_type = "t2.micro"
+  subnet_id                   = aws_subnet.public_b.id
+  vpc_security_group_ids      = [aws_security_group.allow_ssh_pub.id]
+  key_name      = aws_key_pair.keypair.keypair
+
+  tags = {
+    Name = "Bastion Host B"
+  }
+}
+
+resource "aws_instance" "bastion_c" {
+  ami           = "ami-033b95fb8079dc481"
+  associate_public_ip_address = true
+  instance_type = "t2.micro"
+  subnet_id                   = aws_subnet.public_c.id
+  vpc_security_group_ids      = [aws_security_group.allow_ssh_pub.id]
+  key_name      = aws_key_pair.keypair.keypair
+
+  tags = {
+    Name = "Bastion Host C"
+  }
 }
