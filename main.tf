5a80ac7366aad77d6198a77c476830cdd8391fba
nikhiljsk <nikhiljsk98@gmail.com>
2022-02-18 02:07:51 +0530

    Changes

--- main.tf
+++ main.tf
@@ -1,5 +1,5 @@
 provider "aws" {
-  region = "us-east-2"
+  region = "us-east-1"
 }
 
 # VPC
@@ -15,7 +15,7 @@ resource "aws_vpc" "main" {
 resource "aws_subnet" "public_a" {
   vpc_id     = aws_vpc.main.id
   cidr_block = "10.0.1.0/24"
-  availability_zone = "us-east-2a"
+  availability_zone = "us-east-1a"
 
   tags = {
     Name = "Public Subnet A"
@@ -25,7 +25,7 @@ resource "aws_subnet" "public_a" {
 resource "aws_subnet" "private_a" {
   vpc_id     = aws_vpc.main.id
   cidr_block = "10.0.15.0/24"
-  availability_zone = "us-east-2a"
+  availability_zone = "us-east-1a"
 
   tags = {
     Name = "Private Subnet A"
@@ -35,7 +35,7 @@ resource "aws_subnet" "private_a" {
 resource "aws_subnet" "public_b" {
   vpc_id     = aws_vpc.main.id
   cidr_block = "10.0.2.0/24"
-  availability_zone = "us-east-2b"
+  availability_zone = "us-east-1b"
 
   tags = {
     Name = "Public Subnet B"
@@ -45,7 +45,7 @@ resource "aws_subnet" "public_b" {
 resource "aws_subnet" "private_b" {
   vpc_id     = aws_vpc.main.id
   cidr_block = "10.0.16.0/24"
-  availability_zone = "us-east-2b"
+  availability_zone = "us-east-1b"
 
   tags = {
     Name = "Private Subnet B"
@@ -55,7 +55,7 @@ resource "aws_subnet" "private_b" {
 resource "aws_subnet" "public_c" {
     vpc_id     = aws_vpc.main.id
     cidr_block = "10.0.3.0/24"
-    availability_zone = "us-east-2c"
+    availability_zone = "us-east-1c"
   
     tags = {
       Name = "Public Subnet C"
@@ -65,15 +65,14 @@ resource "aws_subnet" "public_c" {
   resource "aws_subnet" "private_c" {
     vpc_id     = aws_vpc.main.id
     cidr_block = "10.0.17.0/24"
-    availability_zone = "us-east-2c"
+    availability_zone = "us-east-1c"
   
     tags = {
       Name = "Private Subnet C"
     }
   }
 
-# Main Internal Gateway for VPC
-# https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/internet_gateway
+# Internet Gateway for VPC
 resource "aws_internet_gateway" "igw" {
   vpc_id = aws_vpc.main.id
 
@@ -83,28 +82,60 @@ resource "aws_internet_gateway" "igw" {
 }
 
 # Elastic IP for NAT Gateway
-# https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/eip
-resource "aws_eip" "nat_eip" {
+resource "aws_eip" "nat_eip_a" {
   vpc        = true
   depends_on = [aws_internet_gateway.igw]
   tags = {
-    Name = "NAT Gateway EIP"
+    Name = "NAT EIP A"
   }
 }
 
-# Main NAT Gateway for VPC
-# https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/nat_gateway
-resource "aws_nat_gateway" "nat" {
-  allocation_id = aws_eip.nat_eip.id
-  subnet_id     = aws_subnet.public.id
+resource "aws_eip" "nat_eip_b" {
+  vpc        = true
+  depends_on = [aws_internet_gateway.igw]
+  tags = {
+    Name = "NAT EIP B"
+  }
+}
+
+resource "aws_eip" "nat_eip_c" {
+  vpc        = true
+  depends_on = [aws_internet_gateway.igw]
+  tags = {
+    Name = "NAT EIP C"
+  }
+}
+
+# NAT Gateways for VPC
+resource "aws_nat_gateway" "nat_a" {
+  allocation_id = aws_eip.nat_eip_a.id
+  subnet_id     = aws_subnet.public_a.id
 
   tags = {
-    Name = "Main NAT Gateway"
+    Name = "NAT Gateway A"
   }
 }
 
+resource "aws_nat_gateway" "nat_b" {
+  allocation_id = aws_eip.nat_eip_b.id
+  subnet_id     = aws_subnet.public_b.id
+
+  tags = {
+    Name = "NAT Gateway B"
+  }
+}
+
+resource "aws_nat_gateway" "nat_c" {
+  allocation_id = aws_eip.nat_eip_c.id
+  subnet_id     = aws_subnet.public_c.id
+
+  tags = {
+    Name = "NAT Gateway C"
+  }
+}
+
+
 # Route Table for Public Subnet
-# https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/route_table
 resource "aws_route_table" "public" {
   vpc_id = aws_vpc.main.id
 
@@ -118,31 +149,73 @@ resource "aws_route_table" "public" {
   }
 }
 
-# Association between Public Subnet and Public Route Table
-# https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/route_table_association
-resource "aws_route_table_association" "public" {
-  subnet_id      = aws_subnet.public.id
+# Association between 3 Public Subnet and 1 Public Route Table
+resource "aws_route_table_association" "public_a" {
+  subnet_id      = aws_subnet.public_a.id
   route_table_id = aws_route_table.public.id
 }
 
+resource "aws_route_table_association" "public_b" {
+  subnet_id      = aws_subnet.public_b.id
+  route_table_id = aws_route_table.public.id
+}
+
+resource "aws_route_table_association" "public_c" {
+  subnet_id      = aws_subnet.public_c.id
+  route_table_id = aws_route_table.public.id
+}
+
+
 # Route Table for Private Subnet
-# https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/route_table
-resource "aws_route_table" "private" {
+resource "aws_route_table" "private_a" {
   vpc_id = aws_vpc.main.id
 
   route {
     cidr_block = "0.0.0.0/0"
-    gateway_id = aws_nat_gateway.nat.id
+    gateway_id = aws_nat_gateway.nat_a.id
   }
 
   tags = {
-    Name = "Private Route Table"
+    Name = "Private Route Table A"
+  }
+}
+
+resource "aws_route_table" "private_b" {
+  vpc_id = aws_vpc.main.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_nat_gateway.nat_b.id
+  }
+
+  tags = {
+    Name = "Private Route Table B"
+  }
+}
+
+resource "aws_route_table" "private_c" {
+  vpc_id = aws_vpc.main.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_nat_gateway.nat_c.id
+  }
+
+  tags = {
+    Name = "Private Route Table C"
   }
 }
 
 # Association between Private Subnet and Private Route Table
-# https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/route_table_association
-resource "aws_route_table_association" "private" {
-  subnet_id      = aws_subnet.private.id
-  route_table_id = aws_route_table.private.id
+resource "aws_route_table_association" "private_a" {
+  subnet_id      = aws_subnet.private_a.id
+  route_table_id = aws_route_table.private_a.id
+}
+resource "aws_route_table_association" "private_b" {
+  subnet_id      = aws_subnet.private_b.id
+  route_table_id = aws_route_table.private_b.id
+}
+resource "aws_route_table_association" "private_c" {
+  subnet_id      = aws_subnet.private_c.id
+  route_table_id = aws_route_table.private_c.id
 }
