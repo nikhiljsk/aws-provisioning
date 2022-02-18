cee74da0de889ba2a66459a3b94421aaf41b17b9
nikhiljsk <nikhiljsk98@gmail.com>
2022-02-18 13:35:43 +0530

    Nikhi JSK

--- main.tf
+++ main.tf
@@ -352,7 +352,6 @@ resource "aws_launch_template" "template" {
   instance_type          = "t2.micro"
   key_name               = aws_key_pair.keypair.key_name
   vpc_security_group_ids = [aws_security_group.allow_ssh_priv.id]
-  user_data              = filebase64("./run_http.sh")
 
   tags = {
     Name = "Template"
--- run_http.sh
+++ run_http.sh
@@ -4,4 +4,5 @@ sudo yum update -y
 sudo yum install -y httpd
 sudo systemctl start httpd
 sudo systemctl enable httpd
+sudo systemctl status httpd
 sudo echo '<center><h1>HTTP Server is Running! This instance is in the private subnet </h1></center>' > /var/www/html/index.html
